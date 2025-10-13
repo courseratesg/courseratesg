f9975b394117b3c3fa4e24ba65e2b1571583c634
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-10-13 23:06:55 +0800

    infrastructure/ecs: disable container insights to save costs

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -40,11 +40,6 @@ resource "aws_ecs_cluster" "main" {
   }
 
   name = "${var.project_name}-ecs-cluster"
-
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
 }
 
 resource "aws_iam_role" "ecs_task_execution" {
